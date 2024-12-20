f51d4cfe9805979eaf0c81b4c04c8264962336e2
BIOCADO <jonatasfelipe24@gmail.com>
2024-12-19 21:42:34 -0300

    Modificação nos arquivos
    Renomeei a pasta e documentei o código

---
+++ Introdução/ex002.py
@@ -0,0 +1,3 @@
+#ultilizando a função input 
+nome = input('Digite seu nome: ') #Serve para interação com o usuário, fazendo com que o mesmo insira dados
+print('É um prazer te conhecer, {}!'.format(nome)) #Formatação no print
